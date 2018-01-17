5059ac285e8120344bd263c9d985556874708130
iugo <iugogogo@gmail.com>
2018-01-17 11:39:39 +0800

    add some console out

--- src/includes/proxy.ts
+++ src/includes/proxy.ts
@@ -2,8 +2,11 @@ import * as Koa from 'koa';
 import fetch from 'node-fetch';
 
 const proxy = async (ctx: Koa.Context) => {
+  const time = Date.now();
   try {
     const url = `https://${ctx.header.host + ctx.path}?${ctx.querystring}`;
+    console.log(time, '请求 url', url);
+    console.log(time, '请求 body', ctx.request.rawBody);
     const body = await fetch(url, {
       body: ctx.request.rawBody,
       headers: {
@@ -14,10 +17,10 @@ const proxy = async (ctx: Koa.Context) => {
     }).then(res => res.text());
     ctx.body = body;
   } catch (err) {
-    console.error(err);
+    console.error(time, err);
     ctx.body = '"出错了"';
   } finally {
-    console.log('请求执行完毕');
+    console.log(time, '请求执行完毕');
   }
 };
 
